dddd93f717240aaaa909793bca954d5338844a8e
mahdi <mxhdiqaim@gmail.com>
2025-08-07 21:19:34 +0100

    chore(migrations): update migrations path and expand .dockerignore
    - Corrected migrations folder path in `migrateDB` function.
    - Enhanced `.dockerignore` with additional files and patterns for better exclusion coverage.

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -5,9 +5,11 @@ import db, { pool } from "./index";
 const migrateDB = async () => {
     console.log("Migration start");
     const client = await pool.connect();
-    await migrate(db, { migrationsFolder: "./migrations" });
+    await migrate(db, { migrationsFolder: "../../migrations" });
     client.release(true);
     console.log("Migration done");
 };
 
-migrateDB().then(r => console.log(r)).catch(e => console.error(e));
+migrateDB()
+    .then((r) => console.log(r))
+    .catch((e) => console.error(e));
