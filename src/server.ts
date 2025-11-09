cbf5c376b14c3771b13d63f52f68315f32065fec
mahdi <mxhdiqaim@gmail.com>
2025-11-09 22:12:38 +0100

    feat(rate-limiter): add rate limiting middleware to enhance request handling

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,19 @@ import path from "path";
 import configureSession from "./config/session-config";
 import routes from "./routes";
 import { getEnvVariable } from "./utils";
+import { rateLimit } from "express-rate-limit";
 
 export const app = express();
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    standardHeaders: true, // Return rate limit info in the headers
+    legacyHeaders: false, // Disable the X-RateLimit headers
+    message: "Too many requests from this IP, please try again later.",
+});
+
+app.use(limiter);
 
 const NODE_ENV = getEnvVariable("NODE_ENV");
 
