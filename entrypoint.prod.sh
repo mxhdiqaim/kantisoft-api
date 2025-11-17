1f3d003d1234cdb15a731382cf2c2b124a392973
mahdi <mxhdiqaim@gmail.com>
2025-11-17 12:57:11 +0100

    feat(docker): update entrypoint script to align with updated PostgreSQL environment variable naming and secrets

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Use the variables that SHOULD be defined in the Docker Compose API environment block
-DB_USER_FINAL=$DB_USER
-DB_HOST_FINAL=$DB_HOST
-DB_PORT_FINAL=$DB_PORT
+DB_USER_FINAL=$POSTGRES_USER
+DB_HOST_FINAL=$POSTGRES_HOST
+DB_PORT_FINAL=$POSTGRES_PORT
 
 # Check if required environment variables are set
 # Check the host and user/port which are needed for connection
@@ -18,7 +18,7 @@ if [ -z "$REDIS_HOST" ] || [ -z "$REDIS_PORT" ]; then
 fi
 
 # The DB_PASSWORD_FILE_PATH is defined by Docker Secrets: /run/secrets/db_password_file
-if [ -z "$DB_PASSWORD_FILE_PATH" ]; then
+if [ -z "$POSTGRES_PASSWORD_FILE_PATH" ]; then
   echo "Error: DB_PASSWORD_FILE_PATH environment variable is not set."
   exit 1
 fi
@@ -26,15 +26,15 @@ fi
 echo "Waiting for the database at $DB_HOST_FINAL:$DB_PORT_FINAL..."
 
 # Use the secret file content for the password during the readiness check
-export PGPASSWORD=$(cat "$DB_PASSWORD_FILE_PATH")
+export POSTGRES_PASSWORD=$(cat "$POSTGRES_PASSWORD_FILE_PATH")
 
 until pg_isready -h "$DB_HOST_FINAL" -p "$DB_PORT_FINAL" -U "$DB_USER_FINAL"; do
   echo "Database is unavailable - sleeping"
   sleep 1
 done
 
-# Clear the PGPASSWORD environment variable immediately after the check for security
-unset PGPASSWORD
+# Clear the POSTGRES_PASSWORD environment variable immediately after the check for security
+unset POSTGRES_PASSWORD
 
 echo "Postgres is ready."
 
