995a6a4063bfd6ccd61064062193c6b438f11b2f
mahdi <mxhdiqaim@gmail.com>
2025-11-18 10:54:43 +0100

    chore(docker): add healthcheck for api service and update client port configurations

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,8 @@ const URL =
         ? [
               "http://localhost:3000",
               "http://localhost:3001",
-              "http://localhost:8080",
+              "http://localhost:3003",
+              "http://localhost:3004",
           ]
         : [`https://${process.env.CLIENT_DOMAIN}`];
 
