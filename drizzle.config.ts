c9f0fb514f8d856abd5b27220b64045f9cbd0d2d
mahdi <mxhdiqaim@gmail.com>
2025-12-07 23:37:03 +0100

    chore: refactor database credentials setup to use connection string for production environment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,32 +1,21 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-import { readFileSync } from "fs";
 import { getEnvVariable } from "./src/utils";
 
 // Conditionally set dbCredentials based on NODE_ENV
 let dbCredentials;
 const NODE_ENV = getEnvVariable("NODE_ENV");
-// const postgresSsl = getEnvVariable("POSTGRES_SSL_REQUIRED") == "true";
 if (NODE_ENV === "production") {
-    const host = getEnvVariable("DB_HOST");
-    const port = getEnvVariable("DB_PORT");
-    const user = getEnvVariable("DB_USER");
-    const database = getEnvVariable("DB_NAME");
+    const connectionString = getEnvVariable("DB_CONNECTION_STRING");
     const sslRequired = getEnvVariable("DB_SSL_REQUIRED") == "true";
-
-    // Read the password from the secret file
-    const passwordFile = getEnvVariable("DB_PASSWORD_FILE");
-    const password = readFileSync(passwordFile, "utf8").trim();
-
-    // // Read the password from the secret file path
-    // const password = getEnvVariable("DB_PASSWORD_FILE");
+    const dbUrl = new URL(connectionString);
 
     dbCredentials = {
-        host,
-        port: Number(port),
-        user,
-        password,
-        database,
+        host: dbUrl.hostname,
+        port: Number(dbUrl.port),
+        user: dbUrl.username,
+        password: dbUrl.password,
+        database: dbUrl.pathname.replace(/^\//, ""),
         ssl: sslRequired,
     };
 } else {
